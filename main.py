ec48c44ac5de4aa7336940fddd3d37c4f435f5a7
smartinez24 <martinezacademico88@gmail.com>
2023-04-22 01:11:44 -0500

    New query

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 import pandas as pd
 
 app = FastAPI(title= 'Estimación biomasa disponible.', 
-              description= 'Con las imágenes satelitales para obtener el NDVI de un campo, saber cuánto forraje se dispone y por cuánto tiempo.')
+              description= 'Con las imágenes satelitales para obtener el NDVI de un campo, saber cuánto forraje se dispone y por cuánto tiempo, para razas Británicas entre los 450 y 500 kg, en un lote determinado.')
 
 df = pd.read_csv('./dataset/All_Harvest.csv').set_index('Fecha')
 df.dropna(how='all', inplace=True)
@@ -67,16 +67,66 @@ async def Biomasa_y_Pastoreo_por_campo(Id_lote: int, Cow_number: int, Type_culti
     df_ndvi['biomass_mean'] = round((df_ndvi['ndvi_mean'] * Biomasa_max) * area, 1)
     biomass = df_ndvi['biomass_mean'].values[0]
     
-    time = biomass / total_ration
+    time = round(biomass / total_ration)
+
+    biomass_20 = biomass * .80
+    time_20 = round(biomass_20 / total_ration)
+
+    biomass_50 = biomass * .50
+    time_50 = round( biomass_50 / total_ration)
+
+    biomass_80 = biomass * .20
+    time_80 = round( biomass_80 / total_ration)
+
+    water = Cow_number * 100
+    total_water = water * round(time)
 
     ans = {'Id_lote': Id_lote,
            'Nombre del lote': name_field, 
            'Campo al que pertenece': name_farm,
-           'Área': round(area, 3), 
+           'Área en hectáreas': round(area, 3), 
            'Tipo de cultivo': cultivo,
            'Total animales': Cow_number,
+           'Total de agua por día en litros': water,
+           'Total de agua para los días de pastoreo estimados': total_water,
            'Biomasa del lote en kg': biomass, 
-           'Días de pastoreo estimados': round(time)}
+           'Días de pastoreo estimados': time,
+           f'Días de pastoreo estimados con el 20% ({round(biomass_20, 1)} kg) de forraje consumido': time_20,
+           f'Días de pastoreo estimados con el 50% ({round(biomass_50, 1)} kg) de forraje consumido': time_50,
+           f'Días de pastoreo estimados con el 80% ({round(biomass_80, 1)} kg) de forraje consumido': time_80}
+    
+    global biomasa_lote
+    biomasa_lote = biomass 
 
     return {"Información del lote": ans}
-    
+
+
+@app.get('/Cantidad de animales ideales', description= 'Ingrese por favor la cantidad de días en los que pretende dejar el ganado en .')
+async def Ideal_de_animales(dias: int):
+
+    global biomasa_lote
+
+    time_per_ration = dias * 15 
+
+    animals = round(biomasa_lote / time_per_ration)
+    total_ration = 15 * animals
+
+    biomasa_lote_20 = biomasa_lote * .80 
+    time_20 = round( biomasa_lote_20 / total_ration)
+
+    biomasa_lote_50 = biomasa_lote * .50
+    time_50 = round( biomasa_lote_50 / total_ration)
+
+    biomasa_lote_80 = biomasa_lote * .20 
+    time_80 = round( biomasa_lote_80 / total_ration)
+
+    water = animals * 100
+
+    ans = { 'Biomasa del lote en kg': biomasa_lote,
+            f'Cantidad de animales ideales para {dias} días de pastoreo': animals,
+            'Total de agua por día en litros': water,
+            f'Días de pastoreo estimados con el 20% ({round(biomasa_lote_20, 1)} kg) de forraje consumido': time_20,
+            f'Días de pastoreo estimados con el 50% ({round(biomasa_lote_50, 1)} kg) de forraje consumido': time_50,
+            f'Días de pastoreo estimados con el 80% ({round(biomasa_lote_80, 1)} kg) de forraje consumido': time_80} 
+
+    return ans
